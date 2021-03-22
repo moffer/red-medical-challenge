5617bf7bcde68be6651c4b5bf07a8bd9c9dd6c93
Rafael Muselmann <rafael.muselmann@gmx.de>
2021-03-22 09:50:01 +0100

    Add solution word for task 4.

--- source/calculation.ts
+++ source/calculation.ts
@@ -19,3 +19,37 @@ export function calculateNumbers(text: string): { sumOfNumbers: number, vocals: 
     }
     return { sumOfNumbers, vocals };
 }
+
+export function getSolutionWord(text: string) {
+    const sentences = splitTextBySentences(text);
+
+    const calculatedSumsPerSentence = sumPerSentence(sentences);
+    const tenBiggestValues = sortByNumbersDesc(calculatedSumsPerSentence).slice(0, 10);
+
+    const tenBiggestValuesInOrder = calculatedSumsPerSentence.filter((value) => tenBiggestValues.includes(value));
+
+    const tenBiggestValuesInOrderIndexSubtracted = subtractIndexOfNumberArray(tenBiggestValuesInOrder);
+    const solution = convertAsciiToString(tenBiggestValuesInOrderIndexSubtracted);
+    return solution;
+}
+
+function splitTextBySentences(text: string): string[] {
+    return text.split(/\./);
+}
+
+function sumPerSentence(sentences: string[]): number[] {
+    return sentences.map((sentence) => calculateNumbers(sentence).sumOfNumbers);
+}
+
+function sortByNumbersDesc(numberArray: number[]) {
+    // Slice to make a copy, not sorting on original array.
+    return numberArray.slice().sort((a, b) => b - a);
+}
+
+function subtractIndexOfNumberArray(numbers: number[]) {
+    return numbers.map((value, index) => value - index);
+}
+
+function convertAsciiToString(numbers: number[]) {
+    return numbers.map((value) => String.fromCharCode(value)).reduce((prev, char) => prev += char);
+}
